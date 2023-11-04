378afcad3589d649041c2c743034522068b67d26
lar9482 <lar9482@utulsa.edu>
2023-11-04 17:27:16 -0500

    Documenting functions

--- WorldKnown/QValue.py
+++ WorldKnown/QValue.py
@@ -1,5 +1,27 @@
 from MDP.hashStates import hashState
 
+"""
+    Calculates the Q value given the current state and action
+    @param mdp: MDP
+        The MDP simulator
+    
+    @param discount: Float
+        The discount factor
+    
+    @param currState: MDPState
+        The current state
+    
+    @param currAction: Actions
+        The current action
+    
+    @param U: {hashState(state): float}
+        The current utility function
+        NOTE: 
+        The key is a hashed version of 'state' using 'hashState'
+    
+    @returns float:
+        The QValue itself
+"""
 def QValue(mdp, discount, currState, currAction, U):
     nextStateAndProbs = {
         nextState: transProb 
--- WorldKnown/valueIteration.py
+++ WorldKnown/valueIteration.py
@@ -4,6 +4,22 @@ import sys
 from WorldKnown.QValue import QValue
 from MDP.hashStates import hashState
 
+"""
+    Calculates the utility function for the MDP
+    @param mdp: MDP
+        The MDP simulator
+    
+    @param discount: Float
+        The discount factor
+    
+    @param error: Float
+        The maximum error allowed
+    
+    @returns {hashState(state): float}
+        The actual utility function itself
+        NOTE: 
+        The key is a hashed version of 'state' using 'hashState'
+"""
 def valueIteration(mdp, discount, error):
     U = {hashState(state): 0 for state in list(mdp.states)}
     UPrime = {hashState(state): 0 for state in list(mdp.states)}
@@ -14,7 +30,7 @@ def valueIteration(mdp, discount, error):
 
         for state in list(mdp.states):
             hashedState = hashState(state)
-            
+
             UPrime[hashedState] = maxQValue(mdp, discount, state, U)
             if (abs(UPrime[hashedState] - U[hashedState]) > deltaU):
                 deltaU = abs(UPrime[hashedState] - U[hashedState])
@@ -24,6 +40,28 @@ def valueIteration(mdp, discount, error):
 
     return U
 
+
+"""
+    Returns the maximum QValue from the current state given all possible actions
+    one can take from the state.
+
+    @param mdp: MDP
+        The MDP simulator
+    
+    @param discount: Float
+        The discount factor
+    
+    @param currState: MDPState
+        The current state
+    
+    @param currU: {hashState(state): float}
+        A dictionary representing the utility function
+        NOTE: 
+        The key is a hashed version of 'state' using 'hashState'
+    
+    @returns float:
+        The maximum Q value itself
+"""
 def maxQValue(mdp, discount, currState, currU):
     actionsAtCurrState = mdp.actions_at(currState)
     maxU = -sys.maxsize
