bfc819151a919d2ae26a112edd1d8b8d6ff36852
larunnel161 <larunnel161@gmail.com>
2023-11-05 01:36:40 -0500

    Implemented policy iteration

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from WorldKnown.domains.navigationProblem import getNavigationProblem
 from WorldKnown.domains.wumpusWorld import getWumpusWorld
 
 from WorldKnown.valueIteration import valueIteration
+from WorldKnown.policyIteration import policyIteration
 
 def main():
     gridWorld = getNavigationProblem()
     wumpusWorld = getWumpusWorld()
 
-    discount = 0.1
-    error = 0.01
-    GU = valueIteration(gridWorld, discount, error)
-    WU = valueIteration(wumpusWorld, discount, error)
+    discount = 0.25
+    error = 0.1
+    k = 10
+    gridWorld.display()
+    Gpolicy = policyIteration(gridWorld, discount, k)
+    Wpolicy = policyIteration(wumpusWorld, discount, k)
+
+    # GU = valueIteration(gridWorld, discount, error)
+    # WU = valueIteration(wumpusWorld, discount, error)
     print()
 
 if __name__ == '__main__':
