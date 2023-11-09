4e43c6499d0965eef869feb3c2b10452f6677186
lar9482 <lar9482@utulsa.edu>
2023-11-09 12:17:58 -0600

    Writing documentation for policy iteration

--- WorldKnown/policyIteration.py
+++ WorldKnown/policyIteration.py
@@ -4,6 +4,21 @@ from WorldKnown.QValue import QValue
 from MDP.hashStates import hashState
 
 def policyIteration(mdp, discount, k):
+    """
+        Calculates the optimal policy for the MDP(at every state, what is the optimal action)
+
+        @param mdp:
+            The MDP simulator
+
+        @param discount:
+            The discount factor
+        
+        @param k:
+            The number of times to run policyEvaluation
+
+        @returns {hashedState: Action}
+            - NOTE: hashedState is calculated as hashState(state)
+    """
     U = {hashState(state): 0 for state in list(mdp.states)}
     policy = {
         hashState(state): random.choice(mdp.actions_at(state))
@@ -29,6 +44,25 @@ def policyIteration(mdp, discount, k):
     return policy
 
 def policyEvaluation(U, policy, mdp, discount, k):
+    """
+        Implementation of modified policy evaluation.
+
+        The equation
+        Ui+1(s) ← ∑s′ P(s′ | s, πi(s))[R(s, πi(s), s′) + γ Ui(s′)
+        gets evaluated 'k' times.
+
+        @param U: {hashState(state): Float}
+            The current utility function. This gets updated dynamically in this function
+        
+        @param policy: {hashState(state): Actions}
+            The current policy
+        
+        @param discount: Float
+            The discount factor
+        
+        @param k: Integer.
+            The number of times to evaluate the utility function
+    """
     for _ in range(0, k):
         for currState in list(mdp.states):
             
@@ -51,6 +85,23 @@ def policyEvaluation(U, policy, mdp, discount, k):
             U[hashState(currState)] = updatedU
 
 def argmaxQValue(mdp, discount, currState, U):
+    """
+        Calculates 
+        
+        argmax a ∈ A(s) Q-VALUE(mdp, s, a, U)
+
+        @param MDP
+            MDP Simulator
+        
+        @param currState
+            The current state of the MDP
+
+        @param: U: {hashState(state): Float}
+            The current utility function
+        
+        @returns Action:
+            The action that optimizes Q-VALUE(mdp, s, a, U)
+    """
     actionsAtCurrState = mdp.actions_at(currState)
     maxQValue = -sys.maxsize
     argmaxAction = actionsAtCurrState[0]
