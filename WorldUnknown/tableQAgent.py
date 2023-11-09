29a93f0e59ebfd911c8e058ccdb52362c331e12d
lar9482 <lar9482@utulsa.edu>
2023-11-09 12:07:23 -0600

    Beginning implementation for a table-based Q Agent

---
+++ WorldUnknown/tableQAgent.py
@@ -0,0 +1,74 @@
+from enum import Enum
+from MDP.hashStates import hashState
+
+import sys
+import copy
+
+class agentType(Enum):
+    QLearn = 0
+    SASRA = 1
+
+class tableQAgent:
+    def __init__(self, mdpSimulator, agentType, maxExpectedReward, maxNumTries):
+        self.mdpSimulator = mdpSimulator
+        self.agentType = agentType
+        self.N = {}
+        self.Q = {}
+
+        self.prevState = None
+        self.prevAction = None
+
+        self.maxExpectedReward = maxExpectedReward
+        self.maxNumTries = maxNumTries
+
+    def learn(self, currState, currReward):
+        if (self.prevState != None):
+            pass
+
+        self.prevState = currState
+        self.prevAction = self.argMaxExplore(currState)
+
+        return self.prevAction
+
+    def argMaxExplore(self, currState):
+        possibleActions = self.mdpSimulator.actions_at(currState)
+        argmaxAction = None
+        maxExploreValue = -sys.maxsize
+
+        for actionPrime in possibleActions:
+            exploreValuePrime = self.explore(
+                self.lookUpQTable(self.prevState, actionPrime),
+                self.lookUpNTable(self.prevState, actionPrime)
+            )
+            if (exploreValuePrime > maxExploreValue):
+                maxExploreValue = exploreValuePrime
+                argmaxAction = actionPrime
+
+        return argmaxAction
+    
+    def explore(self, U, N):
+        if (N < self.maxNumTries):
+            return self.maxExpectedReward
+        else:
+            return U
+    
+    def incrementNTable(self):
+        hashedPrevState = hashState(self.prevState)
+        self.N[(hashedPrevState, copy.deepcopy(self.prevAction))] += 1
+
+    def lookUpNTable(self, state, action):
+        hashedState = hashState(state)
+        if (self.N.get((hashedState, action)) == None):
+            self.N[(hashedState, action)] = 0
+
+        return self.N[(hashedState, action)]
+    
+    def lookUpQTable(self, state, action):
+        hashedState = hashState(state)
+        if (self.Q.get((hashedState, action)) == None):
+            self.Q[(hashedState, action)] = 0
+        
+        return self.Q[(hashedState, action)]
+    
+    def learningRate(self, N):
+        return 1 / N
