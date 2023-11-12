0f9c0cf744432ae34eade08a61604b417a398d14
larunnel161 <larunnel161@gmail.com>
2023-11-12 00:11:09 -0600

    1. Creating an interface for Q agents 2. Changing other methods to private in tableQAgent

---
+++ WorldUnknown/QAgent.py
@@ -0,0 +1,25 @@
+class QAgent:
+    def learn(self, currState, currReward):
+        """
+            The main source where the agent will learn
+        """
+        pass
+
+    def QLearn(self, state, reward):
+        """
+            Implementation of QLearning with some form of
+
+            Q[s, a] ← Q[s, a] + α(Nsa[s, a])(r + γ maxa′ Q[s′, a′] − Q[s, a])
+        """
+        pass
+
+    def SARSA(self, state, reward):
+        """
+            Implementation of SARSA with some form of 
+
+            Q(s, a) ← Q(s, a) + α[R(s, a, s′) + γ Q(s′, a′) − Q(s, a)]
+        """
+        pass
+
+    def learningRate(self, N):
+        return 1 / N
--- WorldUnknown/tableQAgent.py
+++ WorldUnknown/tableQAgent.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from MDP.hashStates import hashState
-
+from WorldUnknown.QAgent import QAgent
 import sys
 import copy
 import random
@@ -9,7 +9,7 @@ class agentType(Enum):
     QLearn = 0
     SASRA = 1
 
-class tableQAgent:
+class tableQAgent(QAgent):
     def __init__(self, mdpSimulator, agentType, discount = 0.1, maxExpectedReward = 1, maxNumTries = 10, epsilon = 0.5):
         self.mdpSimulator = mdpSimulator
         self.agentType = agentType
@@ -26,7 +26,7 @@ class tableQAgent:
 
     def learn(self, currState, currReward):
         if (self.prevState is not None):
-            self.incrementNTable()
+            self.__incrementNTable()
 
             self.Q[(
                 hashState(self.prevState),
@@ -37,49 +37,49 @@ class tableQAgent:
             )
 
         self.prevState = copy.deepcopy(currState)
-        self.prevAction = self.argMaxExplore(currState)
+        self.prevAction = self.__argMaxExplore(currState)
 
         return copy.deepcopy(self.prevAction)
 
     def QLearn(self, state, reward):
         alpha = self.learningRate(
-            self.lookUpNTable(self.prevState, self.prevAction)
+            self.__lookUpNTable(self.prevState, self.prevAction)
         )
         
         maxDiscountedQValue = -sys.maxsize
         for actionPrime in self.mdpSimulator.actions_at(state):
-            currDiscountedQValue = self.lookUpQTable(state, actionPrime)
+            currDiscountedQValue = self.__lookUpQTable(state, actionPrime)
             maxDiscountedQValue = max(maxDiscountedQValue, currDiscountedQValue)
         
         return alpha * (
             reward + 
             (self.discount * maxDiscountedQValue) -
-            self.lookUpQTable(self.prevState, self.prevAction)
+            self.__lookUpQTable(self.prevState, self.prevAction)
         )
 
     def SARSA(self, state, reward):
         alpha = self.learningRate(
-            self.lookUpNTable(self.prevState, self.prevAction)
+            self.__lookUpNTable(self.prevState, self.prevAction)
         )
         
         actionPrime = (
-            self.argMaxExploit(state) if random.uniform(0, 1) < self.epsilon else
-            self.argMaxExplore(state)
+            self.__argMaxExploit(state) if random.uniform(0, 1) < self.epsilon else
+            self.__argMaxExplore(state)
         )
         
         return alpha * (
             reward + 
-            (self.discount * self.lookUpQTable(state, actionPrime)) -
-            self.lookUpQTable(self.prevState, self.prevAction)
+            (self.discount * self.__lookUpQTable(state, actionPrime)) -
+            self.__lookUpQTable(self.prevState, self.prevAction)
         )
 
-    def argMaxExploit(self, currState):
+    def __argMaxExploit(self, currState):
         possibleActions = self.mdpSimulator.actions_at(currState)
         argmaxActions = []
         maxQValue = -sys.maxsize
         for actionPrime in possibleActions:
 
-            QValuePrime = self.lookUpQTable(currState, actionPrime)
+            QValuePrime = self.__lookUpQTable(currState, actionPrime)
             if (QValuePrime > maxQValue):
                 maxQValue = QValuePrime
                 argmaxActions = [actionPrime]
@@ -88,15 +88,15 @@ class tableQAgent:
         
         return random.choice(argmaxActions)
     
-    def argMaxExplore(self, currState):
+    def __argMaxExplore(self, currState):
         possibleActions = self.mdpSimulator.actions_at(currState)
         argmaxActions = []
         maxExploreValue = -sys.maxsize
 
         for actionPrime in possibleActions:
-            exploreValuePrime = self.explore(
-                self.lookUpQTable(currState, actionPrime),
-                self.lookUpNTable(currState, actionPrime)
+            exploreValuePrime = self.__explore(
+                self.__lookUpQTable(currState, actionPrime),
+                self.__lookUpNTable(currState, actionPrime)
             )
             if (exploreValuePrime > maxExploreValue):
                 maxExploreValue = exploreValuePrime
@@ -106,29 +106,26 @@ class tableQAgent:
 
         return random.choice(argmaxActions)
     
-    def explore(self, U, N):
+    def __explore(self, U, N):
         if (N < self.maxNumTries):
             return self.maxExpectedReward
         else:
             return U
     
-    def incrementNTable(self):
+    def __incrementNTable(self):
         hashedPrevState = hashState(self.prevState)
         self.N[(hashedPrevState, self.prevAction)] += 1
 
-    def lookUpNTable(self, state, action):
+    def __lookUpNTable(self, state, action):
         hashedState = hashState(state)
         if (self.N.get((hashedState, action)) == None):
             self.N[(hashedState, action)] = 0
 
         return self.N[(hashedState, action)]
     
-    def lookUpQTable(self, state, action):
+    def __lookUpQTable(self, state, action):
         hashedState = hashState(state)
         if (self.Q.get((hashedState, action)) == None):
             self.Q[(hashedState, action)] = 0
         
         return self.Q[(hashedState, action)]
-    
-    def learningRate(self, N):
-        return 1 / N
