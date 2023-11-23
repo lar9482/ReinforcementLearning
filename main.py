688c8b80e598f92004a22d296c5db516d971641c
lar9482 <lar9482@utulsa.edu>
2023-11-22 20:47:20 -0600

    1. Importing bandit simulator 2. Implemented an initial form of the UCB algorithm

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from testKnownWorlds import testKnownWorlds
 from testUnknownWorldsDiscrete import testUnknownWorldsQLearnDiscrete, testUnknownWorldsSARSADiscrete
 from testUnknownWorldsContinuous import testUnknownWorldsQLearnContinuous, testUnknownWorldsSARSAContinuous
-
+from testMultiArm import testMultiArm
 def main():
     # testKnownWorlds()
     # testUnknownWorldsQLearnDiscrete()
     # testUnknownWorldsSARSADiscrete()
     # testUnknownWorldsQLearnContinuous()
-    testUnknownWorldsSARSAContinuous()
+    # testUnknownWorldsSARSAContinuous()
+    testMultiArm()
     
 if __name__ == '__main__':
     main()
