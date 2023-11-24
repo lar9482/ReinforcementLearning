1814f1121090462c9f76cafac7761f531da7d8f6
lar9482 <lar9482@utulsa.edu>
2023-11-24 15:03:51 -0600

    Updating infrastructure for testing unknown worlds

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ from testUnknownWorldsDiscrete import testUnknownWorldsQLearnDiscrete, testUnkno
 from testUnknownWorldsContinuous import testUnknownWorldsQLearnContinuous, testUnknownWorldsSARSAContinuous
 from testMultiArm import testMultiArm
 
-from testUnknownWorldsContinuous import runUnknownWorldTest, getDataset
-
+from testUnknownWorldsContinuous import runUnknownWorldTest, getDataset, testParameter_UnknownWorld
+from WorldUnknown.domains.world3 import getWorld3Continuous
+from WorldUnknown.domains.world1 import getWorld1Continuous
 def testUnknownWorlds():
     dataset = getDataset()
-    print()
+    parameter = testParameter_UnknownWorld(
+        'world1',
+        getWorld1Continuous(),
+        0.1,
+        1,
+        10,
+        1
+    )
+    runUnknownWorldTest(parameter)
     
 def main():
     # testKnownWorlds()
     # testUnknownWorldsQLearnDiscrete()
     # testUnknownWorldsSARSADiscrete()
-    testUnknownWorldsQLearnContinuous()
+    # testUnknownWorldsQLearnContinuous()
     # testUnknownWorldsSARSAContinuous()
-    # testUnknownWorlds()
+    testUnknownWorlds()
     # testMultiArm()
     
 if __name__ == '__main__':
